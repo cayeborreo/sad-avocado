08143a58377dd5df77be94d88893444bda0112b9
cborreo-work <cborreo@medgrocer.com>
2018-08-31 22:16:05 +0800

    app (feat): add bulma, sass, concurrent sass-watcher
    BREAKING CHANGE: to start dev server, run `yarn dev` instead of `yarn start`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./assets/stylesheets/scss/index.scss";
 
 class App extends Component {
   render() {
