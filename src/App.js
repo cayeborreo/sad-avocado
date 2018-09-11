3944610cc925eb46642e7ce3733a50e6103a273a
cborreo-work <cborreo@medgrocer.com>
2018-09-11 22:39:52 +0800

    background images(feat): add random background image

--- src/App.js
+++ src/App.js
@@ -1,49 +1,32 @@
 import React, { Component } from "react";
 import "./assets/stylesheets/scss/index.scss";
-import axios from "axios";
+// import axios from "axios";
+import Background from "./components/Background/Background";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: []
+      image: {}
     };
   }
 
-  componentWillMount() {
-    axios
-      .get("https://picsum.photos/list")
-      .then(response => {
-        getRandomIntInclusive = (min, max) => {
-          min = Math.ceil(min);
-          max = Math.floor(max);
-          return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-        };
-
-        console.log(getRandomIntInclusive(100));
-        this.setState({
-          image: response.data[getRandomIntInclusive(100)]
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  // randomizer = () => {
-  //   return Math.random();
-  // };
+  // componentDidMount() {
+  //   const randomNumber = Math.floor(Math.random() * 100) + 0;
+  //   axios
+  //     .get("https://picsum.photos/list")
+  //     .then(response =>
+  //       this.setState({
+  //         image: response.data[randomNumber]
+  //       })
+  //     )
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // }
 
   render() {
-    return (
-      <React.Fragment>
-        <img
-          src={`http://picsum.photos/${this.state.image.width}/${
-            this.state.image.height
-          }?image=${this.state.image.id}`}
-        />
-      </React.Fragment>
-    );
+    return <Background />;
   }
 }
 
